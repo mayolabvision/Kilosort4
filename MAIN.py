fbe005f2dafda5bddba852ba28a71ea1b11374f1
kendranoneman <knoneman@andrew.cmu.edu>
2024-05-08 17:04:16 -0400

    editing for larger files

--- MAIN.py
+++ MAIN.py
@@ -1,10 +1,17 @@
 import urllib.request
 from pathlib import Path
 from tqdm import tqdm
+from kilosort.io import load_probe
+from kilosort import run_kilosort
+
+dat = 1
 
 # NOTE: Be sure to update this filepath if you want the data downloaded to
 #       a specific location.
-SAVE_PATH = Path('ZFM-02370_mini.imec0.ap.bin')
+if dat==0:
+    SAVE_PATH = Path('ZFM-02370_mini.imec0.ap.bin')
+elif dat==1:
+    SAVE_PATH = Path('/Users/kendranoneman/OneDrive_cmu/neuropixels/Ya_240415_s219_dirmem_withhelp_0003_g0_t0.imec0.ap.bin')
 
 class DownloadProgressBar(tqdm):
     """ from https://stackoverflow.com/a/53877507 """
@@ -21,20 +28,27 @@ def download_url(url, output_path):
 # compressed using mtscomp (https://github.com/int-brain-lab/mtscomp)
 # URL = 'https://ibl.flatironinstitute.org/public/mainenlab/Subjects/ZFM-02370/2021-04-28/001/raw_ephys_data/probe00/_spikeglx_ephysData_g0_t0.imec0.ap.e510da60-53e0-4e00-b369-3ea16c45623a.cbin'
 
-## CROPPED DATASET
-URL = 'http://www.kilosort.org/downloads/ZFM-02370_mini.imec0.ap.bin'
-download_url(URL, SAVE_PATH)
+if dat==0:
+    ## FULL DATASET (download locally then decompress)
+    #compressed using mtscomp (https://github.com/int-brain-lab/mtscomp)
+    URL = 'https://ibl.flatironinstitute.org/public/mainenlab/Subjects/ZFM-02370/2021-04-28/001/raw_ephys_data/probe00/_spikeglx_ephysData_g0_t0.imec0.ap.e510da60-53e0-4e00-b369-3ea16c45623a.cbin'
+    ## CROPPED DATASET
+    #URL = 'http://www.kilosort.org/downloads/ZFM-02370_mini.imec0.ap.bin'
+    #download_url(URL, SAVE_PATH)
 
-# Download channel maps for default probes
-from kilosort.utils import download_probes
-download_probes()
+    from kilosort.utils import download_probes
+    download_probes()
+    
+    settings = {'data_dir': SAVE_PATH.parent, 'n_chan_bin': 385, 'probe_name': 'neuropixPhase3B1_kilosortChanMap.mat'}
+    
+    ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate = \
+        run_kilosort(settings=settings)
 
-from kilosort import run_kilosort
+elif dat==1:
+    from kilosort.io import load_probe
+    probe = load_probe('/Users/kendranoneman/OneDrive_cmu/neuropixels/Ya_240415_s219_dirmem_withhelp_0003_g0_t0.imec0.ap_kilosortChanMap.mat')
 
-# NOTE: 'n_chan_bin' is a required setting, and should reflect the total number
-#       of channels in the binary file. For information on other available
-#       settings, see `kilosort.run_kilosort.default_settings`.
-settings = {'data_dir': SAVE_PATH.parent, 'n_chan_bin': 385}
+    settings = {'data_dir': SAVE_PATH.parent, 'n_chan_bin': 385, 'probe': probe}
 
-ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate = \
-    run_kilosort(settings=settings, probe_name='neuropixPhase3B1_kilosortChanMap.mat')
+    ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate = \
+        run_kilosort(settings=settings, probe=probe)
