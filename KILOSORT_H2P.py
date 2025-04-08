89b27f5839eae1ac632a428f254e1b2a695203d4
knoneman <knoneman@andrew.cmu.edu>
2025-04-08 00:39:13 -0400

    h2p changes

--- KILOSORT_H2P.py
+++ KILOSORT_H2P.py
@@ -4,39 +4,29 @@ from tqdm import tqdm
 import numpy as np
 
 from kilosort import run_kilosort, DEFAULT_SETTINGS
-from kilosort.io import save_preprocessing, load_ops
+from kilosort.io import load_probe, save_preprocessing, load_ops
 
-def kilosort_h2p(save_path):
+def kilosort_h2p(save_path,probe_path):
     SAVE_PATH = Path(save_path)
+    PROBE_PATH = Path(probe_path)
 
-    n_chan = 24
-    chanMap = np.arange(n_chan)
-    kcoords = np.zeros(n_chan)
-    xc = np.ones(n_chan)*7.5
-    yc = np.array([7.5 + 150*i for i in range(n_chan)])
-
-    probe = {
-        'chanMap': chanMap,
-        'xc': xc,
-        'yc': yc[::-1],
-        'kcoords': kcoords,
-        'n_chan': n_chan}
+    probe = load_probe(PROBE_PATH)
 
     settings = DEFAULT_SETTINGS
     settings['probe'] = probe
-    settings['n_chan_bin'] = 24
+    settings['n_chan_bin'] = probe['n_chan']+1
     settings['fs'] = 30000
     settings['data_dir'] = SAVE_PATH.parent
 
-    settings['nskip']
+    #settings['nskip']
 
-    settings['batch_size'] = 60000*2 # 60000
-    settings['nblocks'] = 0 # 1
-    settings['Th_universal'] = 8 # 9
-    settings['Th_learned'] = 8   # 8
+    #settings['batch_size'] = 60000*2 # 60000
+    #settings['nblocks'] = 0 # 1
+    #settings['Th_universal'] = 8 # 9
+    #settings['Th_learned'] = 8   # 8
 
-    settings['min_template_size'] = 10 #10
-    settings['nearest_templates'] = 23 # 100
+    #settings['min_template_size'] = 10 #10
+    #settings['nearest_templates'] = 23 # 100
     print(settings)
     
     kilosort_path = SAVE_PATH.parent / 'kilosort4'
@@ -48,12 +38,9 @@ def kilosort_h2p(save_path):
     if temp_wh_path.exists() and temp_wh_path.is_file():
         temp_wh_path.unlink()   
 
-
     ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = run_kilosort(settings=settings, probe=probe)
 
     ops_path = SAVE_PATH.parent / 'kilosort4' / 'ops.npy'
     ops = load_ops(ops_path)
+    print('%%%%%%%%%%%%%%% WRITING TO TEMP_WH.DAT %%%%%%%%%%%%%%%')
     save_preprocessing(SAVE_PATH.parent / 'temp_wh.dat', ops, bfile_path=SAVE_PATH)
-
-
-
--- h2p_submit.sh
+++ h2p_submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH --nodes=1
-#SBATCH --time=0-01:00:00
+#SBATCH --time=0-06:00:00
 #SBATCH --ntasks-per-node=4
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --cluster=gpu
 #SBATCH --partition=a100
 #SBATCH --constraint=40g
@@ -15,10 +15,13 @@
 module purge
 conda activate /ihome/pmayo/knoneman/.conda/envs/kilosort
 
-SAVE_PATH="/ix1/pmayo/OneDrive/DATA/${1}_${2}_${3}/${1}_${2}_${3}.bin"
+SAVE_PATH="/ix1/pmayo/lab_NHPdata/${1}_g0/${1}_g0_imec${2}/${1}_g0_t0.imec${2}.ap.bin"
+PROBE_PATH="/ix1/pmayo/lab_NHPdata/${1}_g0/${1}_g0_imec${2}/${1}_g0_t0.imec${2}.ap_kilosortChanMap.mat"
+
 echo "SAVE_PATH = $SAVE_PATH"
+echo "PROBE_PATH = $PROBE_PATH"
 
 # Run the KILOSORT_H2P function from the imported file
-python -c "from KILOSORT_H2P import kilosort_h2p; kilosort_h2p('$SAVE_PATH')"
+python -c "from KILOSORT_H2P import kilosort_h2p; kilosort_h2p('$SAVE_PATH','$PROBE_PATH')"
 
 echo "DONE"
